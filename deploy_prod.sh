599b72cdd7248bd11edbf108fb271e79d6b1d5bf
charytecholution <chary@techolution.com>
2017-05-24 20:57:18 +0530

    Update deploy_prod.sh

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-ssh -v root@YOUR_IP_HERE << EOF
+ssh -v ec2-user@ec2-13-126-17-39.ap-south-1.compute.amazonaws.com << EOF
 
 echo '1. Updating sources'
-cd /patch/to/your/repository/
+cd /home/ec2-user/circledemo
 git checkout --force master
 git pull
 
