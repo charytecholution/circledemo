42c75c130a0754831bf35bbd09463047cbfcb29d
charytecholution <chary@techolution.com>
2017-05-24 20:40:40 +0530

    Create deploy_prod.sh

---
+++ deploy_prod.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ssh -v root@YOUR_IP_HERE << EOF
+
+echo '1. Updating sources'
+cd /patch/to/your/repository/
+git checkout --force master
+git pull
+
+echo "2. Restart apache"
+
+echo 'Done!'
+
+EOF
